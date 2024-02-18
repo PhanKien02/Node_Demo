2fb21a5c2d9b4dee816d192a2df6245448216c73
Phan Kien <phankien110302@gmail.com>
2024-02-19 00:27:50 +0700

    feat:auth request

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction, response } from "express";
+import jwt from "jsonwebtoken";
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers['authorization'];
+  if (!token) {
+    res.status(401).json();
+  } else {
+    const accessToken = token.split(" ")[1];
+    const decoded = jwt.verify(accessToken, process.env.ACCESS_SECRET);
+    req["user"] = decoded['id'];
+    next();
+  }
+}
